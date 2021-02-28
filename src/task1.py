ee6428e63887345faa3f7b911f529cff96bcb453
Brendan Duke <brendanw.duke@gmail.com>
2021-02-28 15:12:35 -0500

    MNIST-1D ROC-AUC curves

--- src/task1.py
+++ src/task1.py
@@ -3,6 +3,7 @@ import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import auc, confusion_matrix, roc_curve
 from tensorflow import keras
 
 from mnist1d_utils import make_dataset
@@ -22,16 +23,20 @@ def task1():
     num_correct = 0
     num_correct_per_class = np.zeros(num_classes, dtype=np.int64)
     num_total_per_class = np.zeros(num_classes, dtype=np.int64)
+    y_predicted_scores = []
     for i in range(len(x_test)):
         digit_input = x_test[i : i + 1]
         digit_label = y_test[i : i + 1]
         digit_prediction = model(digit_input).numpy()
+        y_predicted_scores.append(digit_prediction)
+
         digit_prediction = np.argmax(digit_prediction)
         if digit_prediction == digit_label:
             num_correct += 1
             num_correct_per_class[digit_label] += 1
 
         num_total_per_class[digit_label] += 1
+    y_predicted_scores = np.concatenate(y_predicted_scores, axis=0)
     print(f"Accuracy: {num_correct/len(x_test)}")
     classwise_accuracy = num_correct_per_class / num_total_per_class
     print(f"Class-wise ccuracy: {classwise_accuracy}")
@@ -42,6 +47,22 @@ def task1():
     plt.xlabel("Digit")
     plt.ylabel("Accuracy")
     plt.savefig(os.path.join("report", "images", "mnist1d-class-accuracy.png"), dpi=256)
+    plt.clf()
+
+    y_test_onehot = np.zeros((len(x_test), num_classes), dtype=np.int64)
+    for y_idx, y_label in enumerate(y_test):
+        y_test_onehot[y_idx, y_label] = 1
+    for i in range(num_classes):
+        false_pos_rate, true_pos_rate, _ = roc_curve(
+            y_test_onehot[:, i], y_predicted_scores[:, i]
+        )
+        digit_auc = auc(false_pos_rate, true_pos_rate)
+        plt.plot(false_pos_rate, true_pos_rate, label=f"{i} (AUC = {digit_auc:.4f})")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("MNIST-1D ROC-AUC Curves")
+    plt.legend(loc="lower right")
+    plt.savefig(os.path.join("report", "images", "mnist1d-roc-curve.png"), dpi=256)
     plt.clf()
 
 
